0e3f4cc07201af11438d184ab9de4a59049990cb
Michael Ha <michael.ha@outlook.com.au>
2020-04-05 12:29:22 -0700

    Add functional tests for person controller.

---
+++ Railtown.API.FunctionTests/Controllers/PersonsControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Railtown.API.FunctionTests.Controllers
+{
+    public class PersonsControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public PersonsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/Person")]
+        public async Task Get_Person_ReturnSuccessStatus(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            //string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            
+        }
+    }
+}
